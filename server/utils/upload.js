c2a71d9bb63f0008dfd75bb6360611d16fba8bc6
Touqeer Ansari <touqeeransari001@gmail.com>
2023-10-31 22:22:31 +0530

    updated

--- server/utils/upload.js
+++ server/utils/upload.js
@@ -1,45 +1,25 @@
-import multer from 'multer';
-import { GridFsStorage } from 'multer-gridfs-storage';
-import dotenv from 'dotenv';
+import multer from "multer";
+import { GridFsStorage } from "multer-gridfs-storage";
+import dotenv from "dotenv";
 dotenv.config();
-const username=process.env.DB.USERNAME;
-const password=process.env.DB.PASSWORD;
-
+const username = process.env.DB.USERNAME;
+const password = process.env.DB.PASSWORD;
 
 const storage = new GridFsStorage({
-    //url: `mongodb://user:codeforinterview@blogweb-shard-00-00.ch1hk.mongodb.net:27017,blogweb-shard-00-01.ch1hk.mongodb.net:27017,blogweb-shard-00-02.ch1hk.mongodb.net:27017/BLOG?ssl=true&replicaSet=atlas-lhtsci-shard-0&authSource=admin&retryWrites=true&w=majority`,
-    url:`mongodb+srv://touqeeransari:Ansari@blog-web-app.kfdmxqd.mongodb.net/?retryWrites=true&w=majority`,
-    options: { useNewUrlParser: true },
-    file: (request, file) => {
-        const match = ["image/png", "image/jpg"];
+  //url: `mongodb://user:codeforinterview@blogweb-shard-00-00.ch1hk.mongodb.net:27017,blogweb-shard-00-01.ch1hk.mongodb.net:27017,blogweb-shard-00-02.ch1hk.mongodb.net:27017/BLOG?ssl=true&replicaSet=atlas-lhtsci-shard-0&authSource=admin&retryWrites=true&w=majority`,
+  url: `mongodb+srv://touqeeransari:Ansari@blog-web-app.kfdmxqd.mongodb.net/?retryWrites=true&w=majority`,
+  options: { useNewUrlParser: true },
+  file: (request, file) => {
+    const match = ["image/png", "image/jpg"];
 
-        if(match.indexOf(file.memeType) === -1) 
-            return`${Date.now()}-blog-${file.originalname}`;
+    if (match.indexOf(file.memeType) === -1)
+      return `${Date.now()}-blog-${file.originalname}`;
 
-        return {
-            bucketName: "photos",
-            filename: `${Date.now()}-blog-${file.originalname}`
-        }
-    }
+    return {
+      bucketName: "photos",
+      filename: `${Date.now()}-blog-${file.originalname}`,
+    };
+  },
 });
 
-export default multer({storage}); 
-
-// const storage=new GridFsStorage({
-//     url:'mongodb+srv://touqeeransari:Ansari@blog-web-app.kfdmxqd.mongodb.net/?retryWrites=true&w=majority',
-//     // url:`mongodb+srv:/${username}:${password}@blog-web-app.kfdmxqd.mongodb.net/?retryWrites=true&w=majority`,
-//     options: {useNewUrlParser: true},
-//     file:(request,file)=>{
-//         const match=["image/png","image=/jpg"];
-//         if(match.indexOf(file.memeType)===-1){
-//             return `${Date.now()}-blob-${file.originalname}`;
-//         }
-//         return{
-//             bucketName:"photos",
-//             filename:`${Date.now()}-blob-${file.originalname}`
-//         }
-//     }
-
-    
-// })
-// export default multer({storage});
+export default multer({ storage });
