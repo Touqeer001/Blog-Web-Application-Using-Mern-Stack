0284d1d3dc8302a3df2ed27d3ded5eb4980052eb
Touqeer Ansari <touqeeransari001@gmail.com>
2023-10-31 22:12:32 +0530

    updated About.js

--- client/src/components/about/About.jsx
+++ client/src/components/about/About.jsx
@@ -1,87 +1,89 @@
-
-import { Box, styled, Typography, Link } from '@mui/material';
-import { GitHub, Instagram, Email } from '@mui/icons-material';
+import { Box, styled, Typography, Link } from "@mui/material";
+import { GitHub, Instagram, Email } from "@mui/icons-material";
 
 const Banner = styled(Box)`
-    background-image: url(https://www.wallpapertip.com/wmimgs/23-236943_us-wallpaper-for-website.jpg);
-    width: 100%;
-    height: 50vh;
-    background-position: left 0px bottom 0px;
-    background-size: cover;
+  background-image: url(https://www.wallpapertip.com/wmimgs/23-236943_us-wallpaper-for-website.jpg);
+  width: 100%;
+  height: 50vh;
+  background-position: left 0px bottom 0px;
+  background-size: cover;
 
-
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `;
 
 const Wrapper = styled(Box)`
-    padding: 20px;
-    & > h3, & > h5 {
-        margin-top: 50px;
-    }
+  padding: 20px;
+  & > h3,
+  & > h5 {
+    margin-top: 50px;
+  }
 `;
 
 const Text = styled(Typography)`
-    color: white;
+  color: white;
 `;
-const Heading=styled(Typography)`
-color: white;
-text-align: center;
-font-size: 56px;
-font-family: cursive;
-font-weight: revert;
-font-style: oblique;
-
-`
-const AboutHeading=styled(Typography)`
-font-size: 20px;
-background: #FFFFFF;
-align-items: center;
-justify-content: center;
-margin-bottom: 21px;
-border-radius: 23px;
-font-size: 60px;
-padding: 0 13px;
-font-family: cursive;
-font-style: italic;
-font-weight: 500;
-
-
-`
+const Heading = styled(Typography)`
+  color: white;
+  text-align: center;
+  font-size: 56px;
+  font-family: cursive;
+  font-weight: revert;
+  font-style: oblique;
+`;
+const AboutHeading = styled(Typography)`
+  font-size: 20px;
+  background: #ffffff;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 21px;
+  border-radius: 23px;
+  font-size: 60px;
+  padding: 0 13px;
+  font-family: cursive;
+  font-style: italic;
+  font-weight: 500;
+`;
 const About = () => {
+  return (
+    <Box>
+      <Banner>
+        {" "}
+        <AboutHeading>Blog For U</AboutHeading>
+      </Banner>
 
-    return (
-        <Box>
-            
-            <Banner> <AboutHeading>Blog For U</AboutHeading></Banner>
-           
-            <Wrapper>
-                <Heading variant="h3">Blog For U</Heading>
-                {/* <Typography>Create Your Blog.Read up to date information in all fields</Typography> */}
-                <Text variant="h5">I'am a full satck devloper Specilaization in MERN Technology.
-      
-                    <Box component="span" style={{ marginLeft: 5 }}>
-                        <Link href="" color="inherit" target="_blank"><GitHub /></Link>
-                    </Box>
-                </Text>
-                <Text variant="h5">
-                   Want to Talk wit me ? Reach out to me on
-                    <Box component="span" style={{ marginLeft: 5 }}>
-                        <Link href="" color="inherit" target="_blank">
-                            <Instagram />
-                        </Link>
-                    </Box>  
-                        or send me an Email 
-                        <Link href="mailto:touqeeransari001@gmail.com?Subject=This is a subject" target="_blank" color="inherit">
-                            <Email />
-                        </Link>.
-                </Text>
-            </Wrapper>
-        </Box>
-    )
-}
+      <Wrapper>
+        <Heading variant="h3">Blog For U</Heading>
+        {/* <Typography>Create Your Blog.Read up to date information in all fields</Typography> */}
+        <Text variant="h5">
+          I'am a full satck devloper Specilaization in MERN Technology.
+          <Box component="span" style={{ marginLeft: 5 }}>
+            <Link href="" color="inherit" target="_blank">
+              <GitHub />
+            </Link>
+          </Box>
+        </Text>
+        <Text variant="h5">
+          Want to Talk wit me ? Reach out to me on
+          <Box component="span" style={{ marginLeft: 5 }}>
+            <Link href="" color="inherit" target="_blank">
+              <Instagram />
+            </Link>
+          </Box>
+          or send me an Email
+          <Link
+            href="mailto:touqeeransari001@gmail.com?Subject=This is a subject"
+            target="_blank"
+            color="inherit"
+          >
+            <Email />
+          </Link>
+        </Text>
+      </Wrapper>
+    </Box>
+  );
+};
 
 export default About;
