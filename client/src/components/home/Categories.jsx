e4cd782ca5d393298d337d6a1fa82880576a45f4
Touqeer Ansari <touqeeransari001@gmail.com>
2023-10-31 22:17:19 +0530

    updated

--- client/src/components/home/Categories.jsx
+++ client/src/components/home/Categories.jsx
@@ -1,76 +1,76 @@
+import {
+  Box,
+  Button,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+  styled,
+} from "@mui/material";
+import { Link, useSearchParams } from "react-router-dom";
 
-import { Box,Button, Table, TableHead, TableRow, TableCell, TableBody, styled } from '@mui/material';
-import { Link, useSearchParams } from 'react-router-dom';
-
-import { categories } from '../../constants/data';
+import { categories } from "../../constants/data";
 
 const StyledTable = styled(Table)`
-    border: 1px solid rgba(224, 224, 224, 1);
-        background: lightgrey;
+  border: 1px solid rgba(224, 224, 224, 1);
+  background: lightgrey;
 `;
-    
+
 const StyledButton = styled(Button)`
-    margin: 20px;
-    width: 85%;
-    background: #6495ED;
-    color: #fff;
-    text-decoration: none;
+  margin: 20px;
+  width: 85%;
+  background: #6495ed;
+  color: #fff;
+  text-decoration: none;
 `;
-    
+
 const StyledLink = styled(Link)`
-    text-decoration: none;
-    color: inherit;
-    font-style: italic;
-    font-family: cursive;
-    font-weight: 900;
+  text-decoration: none;
+  color: inherit;
+  font-style: italic;
+  font-family: cursive;
+  font-weight: 900;
 `;
-const backgroundColor=styled(Box)`
-background: red;
-`
+const backgroundColor = styled(Box)`
+  background: red;
+`;
 
 const Categories = () => {
-    const [searchParams] = useSearchParams();
-    const category = searchParams.get('category');
-    
-    return (
-     
-        <>
-       
-            <Link to={`/create?category=${category || ''}`} style={{ textDecoration: 'none' }}>
-                <StyledButton variant="contained">Create Blog</StyledButton>
-            </Link>
-       
-            
-            <StyledTable>
-                <TableHead>
-                    <TableRow>
-                        <TableCell>
-                            <StyledLink to={"/"}>
-                                All Categories
-                            </StyledLink>
-                        </TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {
-                        categories.map(category => (
-                            <TableRow key={category.id}>
-                                <TableCell>
-                                    <StyledLink to={`/?category=${category.type}`}>
-                                        {category.type}
-                                    </StyledLink>
-                                </TableCell>
-                            </TableRow>
-                        ))
-                    }
-                </TableBody>
-            </StyledTable>
+  const [searchParams] = useSearchParams();
+  const category = searchParams.get("category");
 
-            
-           
-       </>
+  return (
+    <>
+      <Link
+        to={`/create?category=${category || ""}`}
+        style={{ textDecoration: "none" }}
+      >
+        <StyledButton variant="contained">Create Blog</StyledButton>
+      </Link>
 
-    )
-}
+      <StyledTable>
+        <TableHead>
+          <TableRow>
+            <TableCell>
+              <StyledLink to={"/"}>All Categories</StyledLink>
+            </TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {categories.map((category) => (
+            <TableRow key={category.id}>
+              <TableCell>
+                <StyledLink to={`/?category=${category.type}`}>
+                  {category.type}
+                </StyledLink>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </StyledTable>
+    </>
+  );
+};
 
 export default Categories;
