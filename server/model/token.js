546317babe9019f2e687e4c7ab5700f27525da1e
Touqeer Ansari <touqeeransari001@gmail.com>
2023-10-31 22:22:17 +0530

    updated

--- server/model/token.js
+++ server/model/token.js
@@ -1,13 +1,12 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const TokenSchema = mongoose.Schema({
-    token: {
-        type: String,
-        required: true
-    }
+  token: {
+    type: String,
+    required: true,
+  },
 });
 
-
-const token = mongoose.model('token', TokenSchema);
+const token = mongoose.model("token", TokenSchema);
 
 export default token;
