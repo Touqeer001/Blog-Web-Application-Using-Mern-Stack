5ca9fb66a592ff5a95f31c6763e0872e28eef884
Touqeer Ansari <touqeeransari001@gmail.com>
2023-10-31 22:19:31 +0530

    updated

--- client/src/components/home/post/Posts.jsx
+++ client/src/components/home/post/Posts.jsx
@@ -1,48 +1,60 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
-import { Grid, Box } from '@mui/material';
-import { Link, useSearchParams } from 'react-router-dom';
+import { Grid, Box } from "@mui/material";
+import { Link, useSearchParams } from "react-router-dom";
 
 // import { getAllPosts } from '../../../service/api';
-import { API } from '../../../service/api';
+import { API } from "../../../service/api";
 
 //components
-import Post from './Post';
+import Post from "./Post";
 
 const Posts = () => {
-    const [posts, getPosts] = useState([]);
-    
-    const [searchParams] = useSearchParams();
-    const category = searchParams.get('category');
+  const [posts, getPosts] = useState([]);
 
-    useEffect(() => {
-        const fetchData = async () => { 
-            let response = await API.getAllPosts({ category : category || '' });
-            if (response.isSuccess) {
-                getPosts(response.data);
-            }
-        }
-        fetchData();
-    }, [category]);//api call
+  const [searchParams] = useSearchParams();
+  const category = searchParams.get("category");
 
-    return (
-        <>
-      
-            {
-                posts?.length ? posts.map(post => (
-                    <Grid item lg={3} sm={4} xs={12}>
-                        <Link style={{textDecoration: 'none', color: 'inherit'}} to={`details/${post._id}`}> 
-                            <Post post={post} />
-                         </Link>
-                    </Grid>
-                )) : <Box style={{color: '878787', margin: '30px 80px', fontSize: 18,fontStyle: 'italic', color: 'white', fontFamily: 'Roboto', fontWeight: 'bold'}}>
+  useEffect(() => {
+    const fetchData = async () => {
+      let response = await API.getAllPosts({ category: category || "" });
+      if (response.isSuccess) {
+        getPosts(response.data);
+      }
+    };
+    fetchData();
+  }, [category]); //api call
 
-                        No data is available for selected category
-                    </Box>
-            }
-           
-        </>
-    )
-}
+  return (
+    <>
+      {posts?.length ? (
+        posts.map((post) => (
+          <Grid item lg={3} sm={4} xs={12}>
+            <Link
+              style={{ textDecoration: "none", color: "inherit" }}
+              to={`details/${post._id}`}
+            >
+              <Post post={post} />
+            </Link>
+          </Grid>
+        ))
+      ) : (
+        <Box
+          style={{
+            color: "878787",
+            margin: "30px 80px",
+            fontSize: 18,
+            fontStyle: "italic",
+            color: "white",
+            fontFamily: "Roboto",
+            fontWeight: "bold",
+          }}
+        >
+          No data is available for selected category
+        </Box>
+      )}
+    </>
+  );
+};
 
 export default Posts;
