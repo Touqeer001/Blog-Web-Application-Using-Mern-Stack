227670021596a3ab2b9b594f2e011c35d57399ec
Touqeer Ansari <touqeeransari001@gmail.com>
2023-10-31 22:12:52 +0530

    updated Banner.js

--- client/src/components/banner/Banner.jsx
+++ client/src/components/banner/Banner.jsx
@@ -1,14 +1,14 @@
-
-import { styled, Box, Typography } from '@mui/material';
+import { styled, Box, Typography } from "@mui/material";
 
 const Image = styled(Box)`
-    width: 100%;
-    background: url(https://images.pexels.com/photos/1714208/pexels-photo-1714208.jpeg) center/55% repeat-x #000;
-    height: 50vh;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
+  width: 100%;
+  background: url(https://images.pexels.com/photos/1714208/pexels-photo-1714208.jpeg)
+    center/55% repeat-x #000;
+  height: 50vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `;
 
 const Heading = styled(Typography)`
@@ -23,18 +23,19 @@ const Heading = styled(Typography)`
 `;
 
 const SubHeading = styled(Typography)`
-    font-size: 20px;
-    background: #FFFFFF;
+  font-size: 20px;
+  background: #ffffff;
 `;
 
 const Banner = () => {
-    
-    return (
-        <Image>
-            <Heading>BLOG for U.</Heading>
-            <SubHeading>Create Your Own Blog.Read All Types of Blog any Where at any Time.</SubHeading>
-        </Image>
-    )
-}
+  return (
+    <Image>
+      <Heading>BLOG for U.</Heading>
+      <SubHeading>
+        Create Your Own Blog.Read All Types of Blog any Where at any Time.
+      </SubHeading>
+    </Image>
+  );
+};
 
 export default Banner;
