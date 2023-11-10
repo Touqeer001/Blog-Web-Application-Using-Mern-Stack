8f9c0c4ee79579faef9a85b5f3b7c8807b3d09d8
Touqeer Ansari <touqeeransari001@gmail.com>
2023-11-10 20:23:25 +0530

    updated

--- client/src/utils/common-utils.js
+++ client/src/utils/common-utils.js
@@ -1,11 +1,9 @@
 export const getAccessToken = () => {
   return sessionStorage.getItem("accessToken");
 };
-
 export const getRefreshToken = () => {
   return sessionStorage.getItem("refreshToken");
 };
-
 export const setAccessToken = (accessToken) => {
   sessionStorage.setItem("accessToken", `Bearer ${accessToken}`);
 };
