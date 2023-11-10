c30ebfa4923e3843e3fbf0ef5855ed75d6e61ef7
Touqeer Ansari <touqeeransari001@gmail.com>
2023-11-10 20:23:12 +0530

    updated

--- client/src/context/DataProvider.js
+++ client/src/context/DataProvider.js
@@ -1,10 +1,7 @@
 import { createContext, useState } from "react";
-
 export const DataContext = createContext(null);
-
 const DataProvider = ({ children }) => {
   const [account, setAccount] = useState({ name: "", username: "" });
-
   return (
     <DataContext.Provider
       value={{
